d5683babdc112beacd6dbb97bc30f059303e9403
Hans Pabst <hans.pabst@intel.com>
2020-11-19 12:57:28 +0100

    Restore grid code to match upstream.

---
+++ src/grid/common/grid_library.c
@@ -0,0 +1,170 @@
+/*----------------------------------------------------------------------------*/
+/*  CP2K: A general program to perform molecular dynamics simulations         */
+/*  Copyright 2000-2020 CP2K developers group <https://cp2k.org>              */
+/*                                                                            */
+/*  SPDX-License-Identifier: GPL-2.0-or-later                                 */
+/*----------------------------------------------------------------------------*/
+
+#include <assert.h>
+#include <omp.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "grid_common.h"
+#include "grid_constants.h"
+#include "grid_library.h"
+
+static grid_library_globals **per_thread_globals = NULL;
+static bool library_initialized = false;
+static grid_library_config config = {.backend = GRID_BACKEND_AUTO,
+                                     .validate = false};
+
+/*******************************************************************************
+ * \brief Initializes the grid library.
+ * \author Ole Schuett
+ ******************************************************************************/
+void grid_library_init() {
+  if (library_initialized) {
+    printf("Error: Grid library was already initialized.\n");
+    abort();
+  }
+
+  per_thread_globals =
+      malloc(sizeof(grid_library_globals *) * omp_get_max_threads());
+
+// Using parallel regions to ensure memory is allocated near a thread's core.
+#pragma omp parallel default(none) shared(per_thread_globals)
+  {
+    const int ithread = omp_get_thread_num();
+    per_thread_globals[ithread] = malloc(sizeof(grid_library_globals));
+    memset(per_thread_globals[ithread], 0, sizeof(grid_library_globals));
+  }
+
+  library_initialized = true;
+}
+
+/*******************************************************************************
+ * \brief Finalizes the grid library.
+ * \author Ole Schuett
+ ******************************************************************************/
+void grid_library_finalize() {
+  if (!library_initialized) {
+    printf("Error: Grid library is not initialized.\n");
+    abort();
+  }
+
+  for (int i = 0; i < omp_get_max_threads(); i++) {
+    grid_sphere_cache_free(&per_thread_globals[i]->sphere_cache);
+    free(per_thread_globals[i]);
+  }
+  free(per_thread_globals);
+  per_thread_globals = NULL;
+  library_initialized = false;
+}
+
+/*******************************************************************************
+ * \brief Returns a pointer to the thread local sphere cache.
+ * \author Ole Schuett
+ ******************************************************************************/
+grid_sphere_cache *grid_library_get_sphere_cache() {
+  return &per_thread_globals[omp_get_thread_num()]->sphere_cache;
+}
+
+/*******************************************************************************
+ * \brief Configures the grid library.
+ * \author Ole Schuett
+ ******************************************************************************/
+void grid_library_set_config(const int backend, const bool validate) {
+  config.backend = backend;
+  config.validate = validate;
+}
+
+/*******************************************************************************
+ * \brief Returns the library config.
+ * \author Ole Schuett
+ ******************************************************************************/
+grid_library_config grid_library_get_config() { return config; }
+
+/*******************************************************************************
+ * \brief Increment specified counter, see grid_library.h for details.
+ * \author Ole Schuett
+ ******************************************************************************/
+void grid_library_increment_counter(int lp, int kern, int op) {
+  lp = imin(lp, 19);
+  per_thread_globals[omp_get_thread_num()]->counters[lp][kern][op]++;
+}
+
+/*******************************************************************************
+ * \brief Prints statistics gathered by the grid library.
+ * \author Ole Schuett
+ ******************************************************************************/
+void grid_library_print_stats(void (*mpi_sum_func)(long *, int),
+                              const int mpi_comm,
+                              void (*print_func)(char *, int),
+                              const int output_unit) {
+  if (!library_initialized) {
+    printf("Error: Grid library is not initialized.\n");
+    abort();
+  }
+
+  // Sum all counters across threads and mpi ranks.
+  long counters[20][2][2] = {0};
+  double total = 0.0;
+  for (int lp = 0; lp < 20; lp++) {
+    for (int kern = 0; kern < 2; kern++) {
+      for (int op = 0; op < 2; op++) {
+        for (int i = 0; i < omp_get_max_threads(); i++) {
+          counters[lp][kern][op] +=
+              per_thread_globals[i]->counters[lp][kern][op];
+        }
+        mpi_sum_func(&counters[lp][kern][op], mpi_comm);
+        total += counters[lp][kern][op];
+      }
+    }
+  }
+
+  // Print counters.
+  print_func("\n", output_unit);
+  print_func(" ----------------------------------------------------------------"
+             "---------------\n",
+             output_unit);
+  print_func(" -                                                               "
+             "              -\n",
+             output_unit);
+  print_func(" -                                GRID STATISTICS                "
+             "              -\n",
+             output_unit);
+  print_func(" -                                                               "
+             "              -\n",
+             output_unit);
+  print_func(" ----------------------------------------------------------------"
+             "---------------\n",
+             output_unit);
+  print_func(" LP    KERNEL    OPERATION                                     "
+             "COUNT     PERCENT\n",
+             output_unit);
+
+  for (int lp = 0; lp < 20; lp++) {
+    for (int kern = 0; kern < 2; kern++) {
+      for (int op = 0; op < 2; op++) {
+        if (counters[lp][kern][op] == 0)
+          continue; // skip empty counters
+        const char *op_str = (op == 1) ? "collocate" : "integrate";
+        const char *kern_str = (kern == 1) ? "ortho" : "general";
+        char buffer[100];
+        double percent = 100.0 * counters[lp][kern][op] / total;
+        snprintf(buffer, sizeof(buffer), " %-5i %-9s %-12s %38li %10.2f%%\n",
+                 lp, kern_str, op_str, counters[lp][kern][op], percent);
+        print_func(buffer, output_unit);
+      }
+    }
+  }
+
+  print_func(" ----------------------------------------------------------------"
+             "---------------\n",
+             output_unit);
+}
+
+// EOF
---
+++ src/grid/common/grid_library.h
@@ -0,0 +1,80 @@
+/*----------------------------------------------------------------------------*/
+/*  CP2K: A general program to perform molecular dynamics simulations         */
+/*  Copyright 2000-2020 CP2K developers group <https://cp2k.org>              */
+/*                                                                            */
+/*  SPDX-License-Identifier: GPL-2.0-or-later                                 */
+/*----------------------------------------------------------------------------*/
+#ifndef GRID_LIBRARY_H
+#define GRID_LIBRARY_H
+
+#include "grid_sphere_cache.h"
+#include <stdbool.h>
+
+/*******************************************************************************
+ * \brief Initializes the grid library.
+ * \author Ole Schuett
+ ******************************************************************************/
+void grid_library_init();
+
+/*******************************************************************************
+ * \brief Finalizes the grid library.
+ * \author Ole Schuett
+ ******************************************************************************/
+void grid_library_finalize();
+
+/*******************************************************************************
+ * \brief Configuration of the grid library.
+ * \author Ole Schuett
+ ******************************************************************************/
+typedef struct {
+  int backend;   // Selectes the backend to be used by the grid library.
+  bool validate; // When true the reference backend runs in shadow mode.
+} grid_library_config;
+
+/*******************************************************************************
+ * \brief Configures the grid library.
+ * \author Ole Schuett
+ ******************************************************************************/
+void grid_library_set_config(int backend, bool validate);
+
+/*******************************************************************************
+ * \brief Returns the library config.
+ * \author Ole Schuett
+ ******************************************************************************/
+grid_library_config grid_library_get_config();
+
+/*******************************************************************************
+ * \brief Prints statistics gathered by the grid library.
+ * \author Ole Schuett
+ ******************************************************************************/
+void grid_library_print_stats(void (*mpi_sum_func)(long *, int), int mpi_comm,
+                              void (*print_func)(char *, int), int output_unit);
+
+/*******************************************************************************
+ * \brief All exiting counters. When adding a counter also update functions
+ *        internal_add_stats() and grid_library_print_counters().
+ * \author Ole Schuett
+ ******************************************************************************/
+typedef struct {
+  grid_sphere_cache sphere_cache;
+  long counters[20][2][2]; // [lp][kernel][operation]
+} grid_library_globals;
+
+/*******************************************************************************
+ * \brief Returns a pointer to the thread local sphere cache.
+ * \author Ole Schuett
+ ******************************************************************************/
+grid_sphere_cache *grid_library_get_sphere_cache();
+
+/*******************************************************************************
+ * \brief Increment specified counter.
+ * \param lp    Total angular momentum: la_max + lb_max.
+ * \param kern  Kernel: 0=ortho, 1=general.
+ * \param op    Operation: 0=integrate, 1=collocate.
+ * \author Ole Schuett
+ ******************************************************************************/
+void grid_library_increment_counter(int collocate, int ortho, int lp);
+
+#endif // GRID_LIBRARY_H
+
+// EOF
