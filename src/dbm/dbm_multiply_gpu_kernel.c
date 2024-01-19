55364d1ee9a1399991066c0ca42b1d01ae42742a
Hans Pabst <hans.pabst@intel.com>
2024-01-19 15:08:16 +0100

    Disabled debug-print.

--- src/dbm/dbm_multiply_gpu_kernel.c
+++ src/dbm/dbm_multiply_gpu_kernel.c
@@ -44,9 +44,11 @@ void dbm_multiply_gpu_launch_kernel(const offloadStream_t stream,
   assert(0 < m_range[0] && 0 < m_range[1] && m_range[0] <= m_range[1]);
   assert(0 < n_range[0] && 0 < n_range[1] && n_range[0] <= n_range[1]);
   assert(0 < ntasks && NULL != batch && NULL != queue);
+#if 0
   printf("ntasks=%i m=%i..%i n=%i..%i batchsize=%i -> work_size=%i\n", ntasks,
          m_range[0], m_range[1], n_range[0], n_range[1], batchsize,
          (int)work_size);
+#endif
 #if defined(_OPENMP)
 #pragma omp critical(dbm_multiply_gpu_launch_kernel)
 #endif
