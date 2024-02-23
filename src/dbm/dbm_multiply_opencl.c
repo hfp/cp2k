aa5661a88950043d2bc25b6ac25fede53b533341
Hans Pabst <hans.pabst@intel.com>
2024-02-23 13:33:33 +0100

    Consolidated handling errors.

--- src/dbm/dbm_multiply_opencl.c
+++ src/dbm/dbm_multiply_opencl.c
@@ -37,9 +37,9 @@ void dbm_multiply_gpu_launch_kernel(const offloadStream_t stream,
                            ? NULL
                            : &event);
   const c_dbcsr_acc_opencl_stream_t *const str = ACC_OPENCL_STREAM(stream);
-  int batchsize = 1; /* intra-kernel batch-size */
+  int result = EXIT_SUCCESS, batchsize = 1;
   const size_t amount = ntasks, wgsize = 0;
-  const size_t work_size =
+  const size_t work_size = /* consider intra-kernel batchsize */
       dbm_multiply_gpu_worksize(ntasks, m_range[1], &batchsize);
   size_t offset_batch = 0, offset_adata = 0, offset_bdata = 0, offset_cdata = 0;
   c_dbcsr_acc_opencl_info_memptr_t adata, bdata, cdata, batch;
@@ -50,18 +50,18 @@ void dbm_multiply_gpu_launch_kernel(const offloadStream_t stream,
   assert(0 < ntasks && NULL != tasks);
   /* creating/calling kernel must be consistent across threads */
   ACC_OPENCL_ACQUIRE(c_dbcsr_acc_opencl_config.lock_main);
-  OFFLOAD_CHECK(c_dbcsr_acc_opencl_info_devptr_lock(
-      &adata, NULL /*lock*/, pack_a_data, 1 /*esize*/, NULL /*amount*/,
-      &offset_adata));
-  OFFLOAD_CHECK(c_dbcsr_acc_opencl_info_devptr_lock(
-      &bdata, NULL /*lock*/, pack_b_data, 1 /*esize*/, NULL /*amount*/,
-      &offset_bdata));
-  OFFLOAD_CHECK(c_dbcsr_acc_opencl_info_devptr_lock(
-      &cdata, NULL /*lock*/, shard_c_data, 1 /*esize*/, NULL /*amount*/,
-      &offset_cdata));
-  OFFLOAD_CHECK(c_dbcsr_acc_opencl_info_devptr_lock(
+  result |= c_dbcsr_acc_opencl_info_devptr_lock(&adata, NULL /*lock*/,
+                                                pack_a_data, 1 /*esize*/,
+                                                NULL /*amount*/, &offset_adata);
+  result |= c_dbcsr_acc_opencl_info_devptr_lock(&bdata, NULL /*lock*/,
+                                                pack_b_data, 1 /*esize*/,
+                                                NULL /*amount*/, &offset_bdata);
+  result |= c_dbcsr_acc_opencl_info_devptr_lock(&cdata, NULL /*lock*/,
+                                                shard_c_data, 1 /*esize*/,
+                                                NULL /*amount*/, &offset_cdata);
+  result |= c_dbcsr_acc_opencl_info_devptr_lock(
       &batch, NULL /*lock*/, tasks /*batch*/, sizeof(dbm_task_t), &amount,
-      &offset_batch));
+      &offset_batch);
   assert(0 == offset_adata && 0 == offset_bdata && 0 == offset_cdata);
 #if defined(OPENCL_DBM_SOURCE_MULTIPLY_OPENCL)
   if (NULL == kernel) { /* first-time check if kernel is present */
@@ -72,34 +72,35 @@ void dbm_multiply_gpu_launch_kernel(const offloadStream_t stream,
         extensions, sizeof(extensions) / sizeof(*extensions), build_params,
         sizeof(build_params));
     if (0 < nchar && (int)sizeof(build_params) > nchar) {
-      OFFLOAD_CHECK(c_dbcsr_acc_opencl_kernel(
+      result |= c_dbcsr_acc_opencl_kernel(
           0 /*source_is_file*/, OPENCL_DBM_SOURCE_MULTIPLY_OPENCL,
           "dbm_multiply", build_params,
           0 == c_dbcsr_acc_opencl_config.debug
               ? NULL
               : "-cl-fast-relaxed-math -cl-denorms-are-zero",
           NULL /*try*/, NULL /*try_ok*/, extensions,
-          sizeof(extensions) / sizeof(*extensions), &kernel));
+          sizeof(extensions) / sizeof(*extensions), &kernel);
     }
   }
 #else
 #error "OpenCL kernel code not found!"
 #endif
-  OFFLOAD_CHECK(clSetKernelArg(kernel, 0, sizeof(cl_double), &alpha));
-  OFFLOAD_CHECK(clSetKernelArg(kernel, 1, sizeof(cl_int), &m_range[1]));
-  OFFLOAD_CHECK(clSetKernelArg(kernel, 2, sizeof(cl_int), &n_range[1]));
-  OFFLOAD_CHECK(clSetKernelArg(kernel, 3, sizeof(cl_int), &batchsize));
-  OFFLOAD_CHECK(clSetKernelArg(kernel, 4, sizeof(cl_int), &offset_batch));
-  OFFLOAD_CHECK(clSetKernelArg(kernel, 5, sizeof(cl_int), &ntasks));
-  OFFLOAD_CHECK(c_dbcsr_acc_opencl_set_kernel_ptr(kernel, 6, batch.memory));
-  OFFLOAD_CHECK(c_dbcsr_acc_opencl_set_kernel_ptr(kernel, 7, adata.memory));
-  OFFLOAD_CHECK(c_dbcsr_acc_opencl_set_kernel_ptr(kernel, 8, bdata.memory));
-  OFFLOAD_CHECK(c_dbcsr_acc_opencl_set_kernel_ptr(kernel, 9, cdata.memory));
-  OFFLOAD_CHECK(clEnqueueNDRangeKernel(
-      str->queue, kernel, 1 /*work_dim*/, NULL /*offset*/, &work_size,
-      0 != wgsize ? &wgsize : NULL, 0 /*num_wait*/, NULL /*wait_list*/,
-      perf_event));
+  result |= clSetKernelArg(kernel, 0, sizeof(cl_double), &alpha);
+  result |= clSetKernelArg(kernel, 1, sizeof(cl_int), &m_range[1]);
+  result |= clSetKernelArg(kernel, 2, sizeof(cl_int), &n_range[1]);
+  result |= clSetKernelArg(kernel, 3, sizeof(cl_int), &batchsize);
+  result |= clSetKernelArg(kernel, 4, sizeof(cl_int), &offset_batch);
+  result |= clSetKernelArg(kernel, 5, sizeof(cl_int), &ntasks);
+  result |= c_dbcsr_acc_opencl_set_kernel_ptr(kernel, 6, batch.memory);
+  result |= c_dbcsr_acc_opencl_set_kernel_ptr(kernel, 7, adata.memory);
+  result |= c_dbcsr_acc_opencl_set_kernel_ptr(kernel, 8, bdata.memory);
+  result |= c_dbcsr_acc_opencl_set_kernel_ptr(kernel, 9, cdata.memory);
+  result |= clEnqueueNDRangeKernel(str->queue, kernel, 1 /*work_dim*/,
+                                   NULL /*offset*/, &work_size,
+                                   0 != wgsize ? &wgsize : NULL, 0 /*num_wait*/,
+                                   NULL /*wait_list*/, perf_event);
   ACC_OPENCL_RELEASE(c_dbcsr_acc_opencl_config.lock_main);
+  OFFLOAD_CHECK(result);
 }
 
 #endif // defined(__OFFLOAD_OPENCL) && !defined(__NO_OFFLOAD_DBM)
