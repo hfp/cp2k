6a5e62dba32d1b4772b8c8bf6dc6235f2cf67f1b
Hans Pabst <hans.pabst@intel.com>
2024-04-03 14:30:00 +0200

    Disabled WG/SG-variant.

--- src/dbm/dbm_multiply_opencl.c
+++ src/dbm/dbm_multiply_opencl.c
@@ -80,7 +80,7 @@ void dbm_multiply_gpu_launch_kernel(const offloadStream_t stream,
       const char *const split_env = getenv("DBM_MULTIPLY_SPLIT");
       const char *const wg_env = getenv("DBM_MULTIPLY_WG");
       split = (NULL == split_env ? 1 /*default*/ : atoi(split_env));
-      wgsize[0] = (NULL == wg_env ? (wgsize1 * LIBXSMM_ABS(split))
+      wgsize[0] = (NULL == wg_env ? 0 /*(wgsize1 * LIBXSMM_ABS(split))*/
                                   : strtoul(wg_env, NULL, 10));
       if (0 != split && 0 != wgsize2 && 0 < wgsize[0]) { /* subgroups */
         if (LIBXSMM_DELTA(wgsize[0], wgsize1) <=
