068b7cccfe66824d6cb05cb4ed0eadb6ce263537
Hans Pabst <hans.pabst@intel.com>
2024-11-25 13:07:59 +0100

    Cleanup.

--- src/dbm/dbm_multiply.c
+++ src/dbm/dbm_multiply.c
@@ -242,7 +242,6 @@ static void multiply_packs(const bool transa, const bool transb,
 
 #pragma omp parallel reduction(+: flop_sum)
   {
-
     // Blocks are ordered first by shard. Creating lookup tables of boundaries.
 #pragma omp for nowait
     for (int iblock = 1; iblock < pack_a->nblocks; iblock++) {
@@ -263,7 +262,7 @@ static void multiply_packs(const bool transa, const bool transb,
       }
     }
 
-#pragma omp for collapse(2) schedule(dynamic,1) nowait
+#pragma omp for collapse(2) schedule(dynamic,1)
     for (int shard_row = 0; shard_row < nshard_rows; shard_row++) {
       for (int shard_col = 0; shard_col < nshard_cols; shard_col++) {
         const int ishard = shard_row * nshard_cols + shard_col;
