1eaf485f8a96727cfdcb3d8046cd3c84e3c1b904
Hans Pabst <hans.pabst@intel.com>
2025-02-14 13:45:48 +0100

    CMake/LIBXSMM: alias targets in PkgConfig namespace

--- cmake/modules/FindLibXSMM.cmake
+++ cmake/modules/FindLibXSMM.cmake
@@ -15,16 +15,15 @@ set(CP2K_LIBXSMMF_ROOT "${CP2K_LIBXSMM_PREFIX}")
 set(CP2K_LIBXSMMNOBLAS_ROOT "${CP2K_LIBXSMM_PREFIX}")
 
 if(PKG_CONFIG_FOUND)
-  pkg_check_modules(CP2K_LIBXSMM IMPORTED_TARGET GLOBAL libxsmm)
-  pkg_check_modules(CP2K_LIBXSMMEXT IMPORTED_TARGET GLOBAL libxsmmext)
-  pkg_check_modules(CP2K_LIBXSMMF IMPORTED_TARGET GLOBAL libxsmmf)
-  pkg_check_modules(CP2K_LIBXSMMNOBLAS IMPORTED_TARGET GLOBAL libxsmmnoblas)
+  foreach(__lib libxsmm libxsmmf libxsmmext libxsmmnoblas)
+    string(TOUPPER "${__lib}" __lib_search_up)
 
-  # i need to do it twice because of dbcsr build option
-  pkg_check_modules(LIBXSMM QUIET IMPORTED_TARGET GLOBAL libxsmm)
-  pkg_check_modules(LIBXSMMEXT QUIET IMPORTED_TARGET GLOBAL libxsmmext)
-  pkg_check_modules(LIBXSMMF QUIET IMPORTED_TARGET GLOBAL libxsmmf)
-  pkg_check_modules(LIBXSMMNOBLAS QUIET IMPORTED_TARGET GLOBAL libxsmmnoblas)
+    pkg_check_modules(CP2K_${__lib_search_up} IMPORTED_TARGET GLOBAL ${__lib})
+    # need to do it twice because of dbcsr build option
+    pkg_check_modules(${__lib_search_up} QUIET IMPORTED_TARGET GLOBAL ${__lib})
+
+    add_library(cp2k::LibXSMM::${__lib} ALIAS PkgConfig::${__lib_search_up})
+  endforeach()
 endif()
 
 if(NOT CP2K_LIBXSMM_FOUND)
@@ -33,45 +32,6 @@ if(NOT CP2K_LIBXSMM_FOUND)
     string(TOUPPER "LIB${__lib}" __lib_search_up)
     if(NOT CP2K_${__lib_search_up}_FOUND)
       cp2k_find_libraries(${__lib_search_up} ${__lib})
-    endif()
-  endforeach()
-endif()
-
-if(CP2K_LIBXSMM_INCLUDE_DIRS)
-  find_package_handle_standard_args(
-    LibXSMM
-    DEFAULT_MSG
-    CP2K_LIBXSMM_INCLUDE_DIRS
-    CP2K_LIBXSMM_LIBRARY_DIRS
-    CP2K_LIBXSMMNOBLAS_LINK_LIBRARIES
-    CP2K_LIBXSMMEXT_LINK_LIBRARIES
-    CP2K_LIBXSMMF_LINK_LIBRARIES
-    CP2K_LIBXSMM_LINK_LIBRARIES)
-else()
-  cp2k_include_dirs(LIBXSMM "libxsmm.h;include/libxsmm.h")
-  find_package_handle_standard_args(
-    LibXSMM DEFAULT_MSG CP2K_LIBXSMM_LIBRARY_DIRS
-    CP2K_LIBXSMMNOBLAS_LINK_LIBRARIES CP2K_LIBXSMMEXT_LINK_LIBRARIES
-    CP2K_LIBXSMMF_LINK_LIBRARIES CP2K_LIBXSMM_LINK_LIBRARIES)
-endif()
-
-if(NOT TARGET cp2k::LibXSMM::libxsmm)
-  foreach(__lib libxsmm libxsmmf libxsmmext libxsmmnoblas)
-    string(TOUPPER "CP2K_${__lib}" __lib_search_up)
-    if(${__lib_search_up}_FOUND AND NOT TARGET cp2k::LibXSMM::${__lib})
-      add_library(cp2k::LibXSMM::${__lib} INTERFACE IMPORTED)
-    endif()
-
-    set_target_properties(
-      cp2k::LibXSMM::${__lib} PROPERTIES INTERFACE_LINK_LIBRARIES
-                                         "${${__lib_search_up}_LINK_LIBRARIES}")
-
-    if(CP2K_LIBXSMM_INCLUDE_DIRS)
-      set_target_properties(
-        cp2k::LibXSMM::${__lib}
-        PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
-                   "${CP2K_LIBXSMM_INCLUDE_DIRS};${CP2K_LIBXSMM_PREFIX}/include"
-      )
     endif()
   endforeach()
 endif()
